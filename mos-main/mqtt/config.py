bca26c55ac7f441c207dfc588127e36dc21eb2cf
seokkilee <100069256+seokkilee@users.noreply.github.com>
2022-04-22 16:33:55 +0900

    Delete mos-main directory

--- mos-main/mqtt/config.py
+++
@@ -1,8 +0,0 @@
-import os.path
-
-mqtt_broker_host = "127.0.0.1"
-mqtt_broker_port = 1883
-mqtt_keepalive = 60
-
-mqtt_username = "emotion"
-mqtt_passwd = "0808540066"
--- mos-main/mqtt/eclipse-mosquitto/Test.py
+++
@@ -1,2 +0,0 @@
-import vehicle_controller
-
