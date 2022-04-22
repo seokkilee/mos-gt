e8a6407171f1aef3f1713eb8378b1bc067f74a00
seokkilee <100069256+seokkilee@users.noreply.github.com>
2022-04-22 16:34:05 +0900

    Delete mos-prototype_redis directory

--- mos-prototype_redis/camera-c1/README.md
+++
@@ -1,3 +0,0 @@
-# Implementation of:
-* gRPC client to retrieve the objects from gRPC server, implemented by nota
-* redis client to update the retrieved objects to local redis server on C1
--- mos-prototype_redis/mos_common/__init__.py
+++
@@ -1,9 +0,0 @@
-import toml
-import pathlib
-import os
-
-
-with open(
-    os.path.join(pathlib.Path(__file__).parent.resolve(), '../mos.toml'), 'r'
-) as f:
-    MOS_CONFIG = toml.load(f)
